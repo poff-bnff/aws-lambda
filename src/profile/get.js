e7b9cf2ded68b9fdc594ae6e800cc295e18bf5e2
Mariann <tapferm@gmail.com>
2020-09-29 17:05:51 +0300

    Update get.js

--- src/profile/get.js
+++ src/profile/get.js
@@ -1,18 +1,18 @@
 'use strict'
-var AWS = require('aws-sdk')
+const AWS = require('aws-sdk')
 
 module.exports.handler = async (event) => {
-  var cognitoidentityserviceprovider = new AWS.CognitoIdentityServiceProvider({ region: 'eu-central-1' })
-  var params = {
+  const cognitoidentityserviceprovider = new AWS.CognitoIdentityServiceProvider({ region: 'eu-central-1' })
+  const params = {
     AccessToken: ((event.headers.authorization).split(' '))[1] /* required */
   }
   const userDetails = await cognitoidentityserviceprovider.getUser(params).promise()
-  const userProfile = {
-    username: userDetails.Username,
-    name: userDetails.UserAttributes[3].Value,
-    lastname: userDetails.UserAttributes[4].Value,
-    email: userDetails.UserAttributes[5].Value
+  console.log(userDetails)
+  const userProfile = { username: userDetails.Username, profile_filled: 'false' }
 
+  for (const item of userDetails.UserAttributes) {
+    console.log(item)
+    userProfile[item.Name] = item.Value
   }
 
   return userProfile
