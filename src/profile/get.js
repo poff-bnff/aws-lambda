8b4cfebddf6909aeb8e2ffd18308fb42cd105022
SiimS1 <siimsutt@hotmail.com>
2020-09-29 15:06:39 +0300

    userinfo init

--- src/profile/get.js
+++ src/profile/get.js
@@ -1,5 +1,19 @@
 'use strict'
+var AWS = require('aws-sdk')
 
-exports.handler = async () => {
-  return { todo: true }
+module.exports.handler = async (event) => {
+  var cognitoidentityserviceprovider = new AWS.CognitoIdentityServiceProvider({ region: 'eu-central-1' })
+  var params = {
+    AccessToken: ((event.headers.authorization).split(' '))[1] /* required */
+  }
+  const userDetails = await cognitoidentityserviceprovider.getUser(params).promise()
+  const userProfile = {
+    username: userDetails.Username,
+    name: userDetails.UserAttributes[3].Value,
+    lastname: userDetails.UserAttributes[4].Value,
+    email: userDetails.UserAttributes[5].Value
+
+  }
+
+  return userProfile
 }
