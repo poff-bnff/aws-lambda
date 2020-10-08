873fc589625ae6693763909a70ac5626042ad016
Argo Roots <argo@roots.ee>
2020-10-08 20:51:20 +0300

    Use SSM for config

--- src/deploy/post.js
+++ src/deploy/post.js
@@ -4,21 +4,28 @@ const aws = require('aws-sdk')
 
 exports.handler = async (event) => {
   const build = new aws.CodeBuild()
+  const ssm = new aws.SSM()
+
+  const ssmKey = await ssm.getParameter({ Name: 'prod-poff-deploy-key', WithDecryption: true }).promise()
+  const ssmProjects = await ssm.getParameter({ Name: 'prod-poff-deploy-projects', WithDecryption: true }).promise()
+
+  const ssmKeyValue = ssmKey.Parameter.Value
+  const ssmProjectsValue = ssmProjects.Parameter.Value
 
   if (!event.body) {
     return { error: 404 }
   }
 
-  if (event.headers.authorization !== process.env.API_KEY) {
+  if (ssmKeyValue !== (event.headers.authorization || event.headers.Authorization)) {
     return { error: 401 }
   }
 
-  if (process.env.PROJECTS.split(',').includes(event.body)) {
+  if (ssmProjectsValue.split(',').includes(event.body)) {
     return { error: 401 }
   }
 
   const result = await build.startBuild({
-    projectName: 'prod-' + event.body
+    projectName: event.body
   }).promise()
 
   return result
