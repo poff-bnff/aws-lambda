edbe100373c8d39d14b63036ee2a94553c54014f
SiimS1 <siimsutt@hotmail.com>
2020-10-09 19:17:48 +0300

    wrkng

--- src/cognito_triggers/mergeAccount.js
+++ src/cognito_triggers/mergeAccount.js
@@ -2,14 +2,15 @@ var AWS = require('aws-sdk')
 
 exports.handler = async (event) => {
   console.log(event)
-  console.log(event.request.userAttributes.email)
+  let sourceUserUserName = event.userName.split("_")
+  let sourceUserProviderName = (sourceUserUserName[0][0].toUpperCase())+sourceUserUserName[0].slice(1)
   var cognitoidentityserviceprovider = new AWS.CognitoIdentityServiceProvider()
 
   var start = 'email = \"'
   var newUserEmail = event.request.userAttributes.email
   var end = '\"'
   var filter1 = start.concat(newUserEmail, end)
-  console.log(filter1)
+  // console.log(filter1)
 
   var params = {
     UserPoolId: 'eu-central-1_JNcWEm7pr', /* required */
@@ -22,14 +23,37 @@ exports.handler = async (event) => {
 
   const usersList = await cognitoidentityserviceprovider.listUsers(params).promise()
   console.log(usersList)
-  console.log(typeof usersList)
-  console.log(usersList.length)
 
-  if (usersList.length !== 0) {
+  if (usersList.Users.length !== 0) {
     console.log('merge user')
+    console.log(usersList.Users[0].Username)
+    let destinationUserUserName = usersList.Users[0].Username.split("_")
+    let destinationUserProviderName = (destinationUserUserName[0][0].toUpperCase())+destinationUserUserName[0].slice(1)
+
+
+
+
+    var params2 = {
+  DestinationUser: { /* required */
+    ProviderAttributeValue: destinationUserUserName[1],
+    ProviderName: destinationUserProviderName
+  },
+  SourceUser: { /* required */
+    ProviderAttributeName: 'Cognito_Subject',
+    ProviderAttributeValue: sourceUserUserName[1],
+    ProviderName: sourceUserProviderName
+  },
+  UserPoolId: 'eu-central-1_JNcWEm7pr' /* required */
+};
+
+let response = await cognitoidentityserviceprovider.adminLinkProviderForUser(params2).promise()
+console.log(response)
+return event
+
+
   } else {
     event.response.autoConfirmUser = true
+    return event
   }
 
-  // callback(null, event)
 }
