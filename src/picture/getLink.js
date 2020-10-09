69ce633e8fec66d1bfa46288da142dc349af1c6d
SiimS1 <siimsutt@hotmail.com>
2020-10-09 16:43:17 +0300

    pict wrkng

--- src/picture/getLink.js
+++ src/picture/getLink.js
@@ -1,8 +1,16 @@
 'use strict'
 const AWS = require('aws-sdk')
+const s3 = new AWS.S3();
 
 exports.handler = async (event) => {
-  const response = {link: 'hello'}
+  console.log(event)
+
+  var params = {Bucket: 'prod-poff-profile-pictures', Key: 'helloo', ContentType: 'image/png'};
+  var url = s3.getSignedUrl('putObject', params);
+
+  let response = {link: url}
+
+  console.log('response: ' + url)
 
   return response
 }
