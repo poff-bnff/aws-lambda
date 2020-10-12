69c61001d4e6fb5a1a5ef7173dab0e780dca7abf
SiimS1 <siimsutt@hotmail.com>
2020-10-12 13:25:05 +0300

    getFavourites

--- src/favourite/get.js
+++ src/favourite/get.js
@@ -1,5 +1,15 @@
 'use strict'
+const AWS = require('aws-sdk')
 
-exports.handler = async () => {
+exports.handler = async (event) => {
+  var dynamodb = new AWS.DynamoDB()
+
+  const response = await dynamodb.query(params).promise()
+
+  console.log(response)
+
+
+
+
   return { todo: true }
 }
--- src/favourite/put.js
+++ src/favourite/put.js
@@ -1,5 +1,29 @@
 'use strict'
+const AWS = require('aws-sdk')
+const jwt = require('jsonwebtoken')
 
-exports.handler = async () => {
-  return { todo: true }
+exports.handler = async (event) => {
+  console.log(event)
+
+  const accessToken = jwt.decode(((event.headers.authorization).split(' '))[1]) /* required */
+  console.log(accessToken)
+
+  var dynamodb = new AWS.DynamoDB()
+
+  var params = {
+    Item: {
+      cognitoId: {
+        S: accessToken.sub
+      },
+      favouriteFilm: {
+        N: pathParameters.movieId
+      }
+    },
+    TableName: 'favourites2'
+  }
+
+  const response = await dynamodb.putItem(params).promise()
+
+  console.log(response)
+  return response
 }
