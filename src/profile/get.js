3f583b8a942c666eb1ebe3d24d17faa515114d75
Mariann <tapferm@gmail.com>
2020-10-23 18:58:40 +0300

    signup trigger

--- src/profile/get.js
+++ src/profile/get.js
@@ -2,6 +2,9 @@
 const aws = require('aws-sdk')
 
 module.exports.handler = async (event) => {
+
+
+
   const cognitoidentityserviceprovider = new aws.CognitoIdentityServiceProvider({ region: 'eu-central-1' })
   const params = {
     AccessToken: ((event.headers.authorization).split(' '))[1] /* required */
@@ -13,16 +16,36 @@ module.exports.handler = async (event) => {
   for (const item of userDetails.UserAttributes) {
     userProfile[item.Name] = item.Value
   }
-
   if ((userProfile.name.slice((userProfile.name.lastIndexOf(' ')) + 1).localeCompare(userProfile.family_name)) === 0) {
     userProfile.name = userProfile.name.slice(0, userProfile.name.lastIndexOf(' '))
   }
+  let dateCreated
 
-  if (!('birthdate' in userProfile)) {
-    delete userProfile.profile_filled
-  } else {
-    userProfile.profile_filled = 'true'
+  for (let attr of userDetails.UserAttributes){
+    if(attr.Name === "identities"){
+      let identities = JSON.parse(attr.Value)
+      console.log(attr.Value)
+      for (let i of identities){
+        dateCreated = i.dateCreated
+      }
+    }
   }
+  console.log(new Date(dateCreated))
 
+  let now = new Date()
+  console.log(now)
+  now = now.getTime()
+
+  //profiil täidetud
+  if ('birthdate' in userProfile && 'email' in userProfile && 'address' in userProfile && 'family_name' in userProfile && 'name' in userProfile &&  'gender' in userProfile && 'phone_number' in userProfile  ) {
+    userProfile.profile_filled = true
+    console.log(userProfile)
+//registreerinud aga profiil täitmata
+  } else if ('email' in userProfile && 'family_name' in userProfile && 'name' in userProfile){
+    userProfile.profile_filled = false
+  }
+  if(dateCreated - now < 60000){
+    userProfile.profile_filled = 'signup'
+  }
   return userProfile
 }
--- src/profile/post.js
+++ src/profile/post.js
@@ -4,35 +4,21 @@ exports.handler = async (event) => {
   var cognitoidentityserviceprovider = new aws.CognitoIdentityServiceProvider()
 
   const userAttributes = JSON.parse(event.body)
-
   let email
-  // let name
-  // let familyName
   let password
-  // let gender
 
   for (const i of userAttributes) {
     if (i.Name === 'email') {
       console.log('email ' + i.Value)
       email = i.Value
     }
-    // if (i.Name === 'name') {
-    //   console.log('name ' + i.Value)
-    //   name = i.Value
-    // }
-    // if (i.Name === 'family_name') {
-    //   console.log('name ' + i.Value)
-    //   familyName = i.Value
-    // }
+
     if (i.Name === 'password') {
       console.log('password ' + i.Value)
       password = i.Value
       userAttributes.pop(i)
     }
-    // if (i.Name === 'gender') {
-    //   console.log('gender ' + i.Value)
-    //   gender = i.Value
-    // }
+
   }
 
 
@@ -40,40 +26,14 @@ exports.handler = async (event) => {
     ClientId: '38o2sdp2bluc1kik2v4fni1hj2', /* required */
     Password: password, /* required */
     Username: email, /* required */
-    // AnalyticsMetadata: {
-    //   AnalyticsEndpointId: 'STRING_VALUE'
-    // },
-    // ClientMetadata: {
-    //   '<StringType>': 'STRING_VALUE',
-    //   /* '<StringType>': ... */
-    // },
-    // SecretHash: 'STRING_VALUE',
-    // UserAttributes: [{ Name: 'name', Value: name }, { Name: 'family_name', Value: familyName }, { Name: 'gender', Value: gender } ]
+
       UserAttributes: userAttributes
 
-  // UserContextData: {
-  //   EncodedData: 'STRING_VALUE'
-  // },
-  // ValidationData: [
-  //   {
-  //     Name: 'STRING_VALUE', /* required */
-  //     Value: 'STRING_VALUE'
-  //   },
-  //   /* more items */
-  // ]
   }
 
   const response = await cognitoidentityserviceprovider.signUp(params).promise()
 
 
-  // await cognitoidentityserviceprovider.signUp(params, function(err, data) {
-  //   console.log('sees');
-  //   if (err) console.log(err, err.stack); // an error occurred
-  //   else     console.log(data);           // successful response
-  // });
-
   return response
 
-  // event.response.autoConfirmUser = true
-  // callback(null, event)
 }
