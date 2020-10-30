b2743a6653d08acf37dddd3f7b3b84ba238d0105
SiimS1 <siimsutt@hotmail.com>
2020-10-30 19:52:49 +0200

    pictureget using cognito sub

--- src/picture/get.js
+++ src/picture/get.js
@@ -1,11 +1,18 @@
 'use strict'
 const aws = require('aws-sdk')
+const _h = require('../_helpers')
+
 const s3 = new aws.S3()
 
 exports.handler = async (event) => {
   console.log(event)
+    let userId = _h.getUserId(event)
 
-  var params = { Bucket: 'prod-poff-profile-pictures', Key: 'helloo', ContentType: 'image/png' }
+  userId = userId
+
+
+  var params = { Bucket: 'prod-poff-profile-pictures', Key: userId, ContentType: 'image/png' }
+  console.log(params)
   var url = s3.getSignedUrl('putObject', params)
 
   const response = { link: url }
