1fc4347a9acce15079f2fdeb6fb0f186d2e597d4
SiimS1 <siimsutt@hotmail.com>
2020-11-04 13:36:30 +0200

    helper added

--- src/profile/get.js
+++ src/profile/get.js
@@ -1,5 +1,7 @@
 'use strict'
 const aws = require('aws-sdk')
+const _h = require('../_helpers')
+
 
 module.exports.handler = async (event) => {
   const cognitoidentityserviceprovider = new aws.CognitoIdentityServiceProvider({
