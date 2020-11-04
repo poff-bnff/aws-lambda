fed060e684d7dd2d19bbcab838ee68c484c7685b
Mariann <tapferm@gmail.com>
2020-11-04 11:11:56 +0200

    Update get.js

--- src/profile/picture_down/get.js
+++ src/profile/picture_down/get.js
@@ -2,37 +2,31 @@
 const _h = require('../../_helpers')
 const aws = require('aws-sdk')
 
-const getSignedUrl = async (operation, params) => {
-  const s3Endpoint = 'prod-poff-profile-pictures.s3-eu-central-1.amazonaws.com'
-  const s3Bucket = 'prod-poff-profile-pictures'
-  if (!params.Bucket) {
-    params.Bucket = s3Bucket
-  }
-  if (!params.Expires) {
-    params.Expires = 60
-  }
-  return new Promise((resolve, reject) => {
-    let conf
-    if (s3Endpoint) {
-      conf = {
-        endpoint: s3Endpoint,
-        s3BucketEndpoint: true
-      }
-    }
-    aws.config = new aws.Config()
-    aws.config.update({
-      endpoint: s3Endpoint,
-      signatureVersion: 'v4',
-      region: 'eu-central-1'
-    });
-    const s3 = new aws.S3(conf)
-    s3.getSignedUrl(operation, params, (err, url) => {
-      if (err) {
-        return reject(err)
-      }
-      resolve(url)
-    })
+const getSignedUrl = async (userId) => {
+
+  const signedUrlExpireSeconds = 60 * 10
+  const myBucket = 'prod-poff-profile-pictures'
+  const myKey = userId
+  const accsessKeyId = await _h.ssmParameter('prod-poff-s3-key')
+  const secretAccesskey = await _h.ssmParameter('prod-poff-s3-secret')
+
+  const s3 = new aws.S3({
+    accessKeyId: accsessKeyId,
+    signatureVersion: 'v4',
+    region: 'eu-central-1',
+    secretAccessKey: secretAccesskey
+  });
+
+  const url = s3.getSignedUrl('getObject', {
+      Bucket: myBucket,
+      Key: myKey,
+      Expires: signedUrlExpireSeconds
   })
+
+  console.log(url)
+
+  return url
+
 }
 
 
@@ -42,7 +36,7 @@ exports.handler = async (event) => {
 
   let userId = _h.getUserId(event)
 
-  let url = await getSignedUrl('getObject', {Key: userId})
+  let url = await getSignedUrl(userId)
 
   return { url: url }
 
