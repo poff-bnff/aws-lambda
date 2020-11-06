026d3de389ace1036191ae713345c032869e1ba9
SiimS1 <siimsutt@hotmail.com>
2020-11-06 10:51:34 +0200

    consolelogs added to auth get

--- src/auth/facebook/get.js
+++ src/auth/facebook/get.js
@@ -3,9 +3,13 @@
 const _h = require('../../_helpers')
 
 exports.handler = async (event) => {
+  console.log(event)
+
   const domain = await _h.ssmParameter('prod-poff-cognito-domain')
   const clientId = await _h.ssmParameter('prod-poff-cognito-id')
   const referer = _h.getHeader(event, 'referer')
 
-  return _h.redirect(`${domain}/oauth2/authorize?response_type=token&client_id=${clientId}&redirect_uri=${referer}login/&identity_provider=Facebook`)
+  const redirect_uri = (`${domain}/oauth2/authorize?response_type=token&client_id=${clientId}&redirect_uri=${referer}login/&identity_provider=Facebook`)
+  console.log(redirect_uri)
+  return _h.redirect(redirect_uri)
 }
--- src/auth/google/get.js
+++ src/auth/google/get.js
@@ -3,11 +3,11 @@
 const _h = require('../../_helpers')
 
 exports.handler = async (event) => {
+  console.log(event)
+
   const domain = await _h.ssmParameter('prod-poff-cognito-domain')
   const clientId = await _h.ssmParameter('prod-poff-cognito-id')
-
   const referer_host = _h.getRefererHost(event)
-  console.log(referer_host)
 
   const redirect_uri = `${domain}/oauth2/authorize?response_type=token&client_id=${clientId}&redirect_uri=${referer_host}/login/&identity_provider=Google`
   console.log(redirect_uri)
