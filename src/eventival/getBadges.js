25c70858a9de5445f4da0d8d949044f446003b7c
SiimS1 <siimsutt@hotmail.com>
2020-11-15 19:42:51 +0200

    fetch userrights from Eventival

--- src/eventival/getBadges.js
+++ src/eventival/getBadges.js
@@ -3,33 +3,36 @@ const convert = require ('xml-js')
 var op = require('object-path')
 const _h = require('../_helpers')
 
+const timezonestr = ' 00:00:00 GMT+0200'
 
+
+function picAndBadges(xml_str) {
+  let ev_o = op(JSON.parse(convert.xml2json(xml_str, { compact: true })))
+  const pic_and_badges = {
+    photo: ev_o.get(['person', 'photos', 'photo', '_text'], null),
+    badges: ev_o.get('person.badges.badge', [])
+      .filter(badge => badge.cancelled._text === '0')
+      .map(badge => {
+        let cnt = badge.validity_dates.date.length
+        return {
+          valid: {
+            from: badge.validity_dates.date[0]._text + timezonestr,
+            to: badge.validity_dates.date[cnt - 1]._text + timezonestr,
+          },
+          type: badge.badge_type.name._text
+        }
+      })
+  }
+  return pic_and_badges
+}
 
 exports.handler = async (event) => {
   console.log(event)
   const EVENTIVAL_TOKEN = await _h.ssmParameter('prod-poff-eventival-web-token')
 
-  let email = JSON.parse(event.email) || 'tapferm@gmail.com'
+  let email = event.email || 'siimsutt@hotmail.com'
+  console.log(email)
 
-  function picAndBadges(xml_str) {
-    ev_o = op(JSON.parse(convert.xml2json(xml_str, { compact: true })))
-    const pic_and_badges = {
-      photo: ev_o.get(['person', 'photos', 'photo', '_text'], null),
-      badges: ev_o.get('person.badges.badge', [])
-        .filter(badge => badge.cancelled._text === '0')
-        .map(badge => {
-          let cnt = badge.validity_dates.date.length
-          return {
-            valid: {
-              from: badge.validity_dates.date[0]._text,
-              to: badge.validity_dates.date[cnt - 1]._text,
-            },
-            type: badge.badge_type.name._text
-          }
-        })
-    }
-    return pic_and_badges
-  }
 
 
   let dataString = ''
@@ -40,6 +43,7 @@ exports.handler = async (event) => {
           dataString += chunk;
         });
         res.on('end', () => {
+          console.log('dataString ', dataString);
           resolve({
               statusCode: 200,
               body: picAndBadges(dataString)
@@ -55,5 +59,6 @@ exports.handler = async (event) => {
       });
   });
 
-  return response;
+  console.log('response', response)
+  return {response: response}
 }
--- src/profile/get.js
+++ src/profile/get.js
@@ -3,6 +3,16 @@ const aws = require('aws-sdk')
 const _h = require('../_helpers')
 const lambda = new aws.Lambda()
 
+const EVENTIVALBADGEWHITELIST = [
+  'MANAGEMENT',
+  'JURY',
+  'INDUSTRY ACCESS',
+  'INDUSTRY PRO',
+  'GUEST',
+  'STAFF',
+  'VOLUNTEER'
+]
+
 
 module.exports.handler = async (event) => {
   console.log('event ', event)
@@ -15,81 +25,64 @@ module.exports.handler = async (event) => {
     AccessToken: ((event.headers.authorization).split(' '))[1] /* required */
   }
   const userDetails = await cognitoidentityserviceprovider.getUser(params).promise()
-
-
-  var lambdaParams = {
-    FunctionName: 'prod3-poff-api-favourite-get',
-    Payload: JSON.stringify({
-      table: 'prod-poff-favourite',
-      headers: { authorization: ((event.headers.authorization).split(' '))[1] }
-     })
-  }
-  console.log('invokeParams ', lambdaParams)
-
-  const lambdaResponse = await lambda.invoke(lambdaParams).promise()
-  console.log('lambdaResponse ', lambdaResponse)
-
-  const shortlist = JSON.parse(lambdaResponse.Payload).map(id => {
-    return { cassette_id: id }
-  })
-  console.log('shortlist ', shortlist)
-
-
-  var lambdaParams2 = {
-    FunctionName: 'prod3-poff-api-favourite-get',
-    Payload: JSON.stringify({
-      table: 'prod-poff-savedscreenings',
-      headers: { authorization: ((event.headers.authorization).split(' '))[1] }
-    })
-  }
-  console.log('invokeParams ', lambdaParams2)
-
-  const lambdaResponse2 = await lambda.invoke(lambdaParams2).promise()
-  console.log('lambdaResponse ', lambdaResponse2)
-
-  const savedscreenings = JSON.parse(lambdaResponse2.Payload)
-
-  console.log('savedscreenings ', savedscreenings)
-
-  // try {
-    var lambdaParams3 = {
-      FunctionName: 'prod3-poff-api-product-get',
-      Payload: JSON.stringify({
-        headers: { authorization: ((event.headers.authorization).split(' '))[1] }
-      })
-    }
-    console.log('invokeParams ', lambdaParams3)
-
-    const lambdaResponse3 = await lambda.invoke(lambdaParams3).promise()
-    console.log('lambdaResponse ', lambdaResponse3)
-
-    const userpasses = JSON.parse(lambdaResponse3.Payload)
-
-    console.log('userpasses ', userpasses)
-
-  // } catch(err){null}
-
-
+  
   console.log('prindin userDetails')
   console.log(userDetails)
   const userProfile = {
     username: userDetails.Username,
-    shortlist: shortlist,
-    savedscreenings: savedscreenings,
-    userpasses: userpasses
+    shortlist: await getShortlist(event),
+    savedscreenings: await getSavedScreenings(event),
+    userpasses: await getUserPasses(event),
+    industryAccessLevel: false
   }
-  const is_eventival_user = userDetails.identities && userDetails.identities.filter(id => id.providerName === 'Eventival').length > 0
-  if (is_eventival_user) {
-    let lambdaParams = {
-      FunctionName: 'prod3-poff-api-eventival.getBadges',
-      Payload: JSON.stringify({
-        email: userDetails.email
-      })
-    }
-    userProfile.eventivalbadges = await lambda.invoke(lambdaParams).promise()
-  }
+  
   for (const item of userDetails.UserAttributes) {
     userProfile[item.Name] = item.Value
+  }
+
+  if (userProfile.identities){
+  userProfile.identities = JSON.parse(userProfile.identities)
+  }
+
+  try{
+    console.log('userDetails.identities ', userProfile.identities)
+    const is_eventival_user = userProfile.identities && userProfile.identities.filter(id => id.providerName === 'Eventival').length > 0
+    console.log('is_eventival_user ', is_eventival_user)
+
+    if (is_eventival_user) {
+      let lambdaParams4 = {
+        FunctionName: 'prod3-poff-api-eventival-getBadges',
+        Payload: JSON.stringify({
+          email: userProfile.email,
+          headers: { authorization: ((event.headers.authorization).split(' '))[1] }
+        })
+      }
+      
+      console.log('eventivalLambdaParams ', lambdaParams4)
+      let _response = await lambda.invoke(lambdaParams4).promise()
+      console.log(_response)
+      const _responseJson = JSON.parse(_response.Payload)
+      console.log(_responseJson)
+      userProfile.eventivalProfile = _responseJson.response.body
+
+      userProfile.industryAccessLevel = userProfile.eventivalProfile.badges.filter(badge => {
+        console.log({badge});
+        if (!EVENTIVALBADGEWHITELIST.includes(badge.type.toUpperCase())) {
+          return false
+        }
+
+        let from = new Date(badge.valid.from).getTime()
+        let now = new Date().getTime()
+        let to = new Date(badge.valid.to).getTime()
+        if (now < from || to < now){
+          return false
+        }
+
+        return true
+      }).length > 0
+    }
+  } catch(err){
+    console.log(err)
   }
 
   if (userProfile.phone_number) {
@@ -109,3 +102,62 @@ module.exports.handler = async (event) => {
 
   return userProfile
 }
+
+
+async function getUserPasses(event) {
+  var lambdaParams3 = {
+    FunctionName: 'prod3-poff-api-product-get',
+    Payload: JSON.stringify({
+      headers: { authorization: ((event.headers.authorization).split(' '))[1] }
+    })
+  }
+  console.log('invokeParams ', lambdaParams3)
+
+  const lambdaResponse3 = await lambda.invoke(lambdaParams3).promise()
+  console.log('lambdaResponse ', lambdaResponse3)
+
+  const userpasses = JSON.parse(lambdaResponse3.Payload)
+
+  console.log('userpasses ', userpasses)
+  return userpasses
+}
+
+async function getSavedScreenings(event) {
+  var lambdaParams2 = {
+    FunctionName: 'prod3-poff-api-favourite-get',
+    Payload: JSON.stringify({
+      table: 'prod-poff-savedscreenings',
+      headers: { authorization: ((event.headers.authorization).split(' '))[1] }
+    })
+  }
+  console.log('invokeParams ', lambdaParams2)
+
+  const lambdaResponse2 = await lambda.invoke(lambdaParams2).promise()
+  console.log('lambdaResponse ', lambdaResponse2)
+
+  const savedscreenings = JSON.parse(lambdaResponse2.Payload)
+
+  console.log('savedscreenings ', savedscreenings)
+  return savedscreenings
+}
+
+async function getShortlist(event) {
+  var lambdaParams = {
+    FunctionName: 'prod3-poff-api-favourite-get',
+    Payload: JSON.stringify({
+      table: 'prod-poff-favourite',
+      headers: { authorization: ((event.headers.authorization).split(' '))[1] }
+    })
+  }
+  console.log('invokeParams ', lambdaParams)
+
+  const lambdaResponse = await lambda.invoke(lambdaParams).promise()
+  console.log('lambdaResponse ', lambdaResponse)
+
+  const shortlist = JSON.parse(lambdaResponse.Payload).map(id => {
+    return { cassette_id: id }
+  })
+  console.log('shortlist ', shortlist)
+  return shortlist
+}
+
